b7fdbe299efbe68c1571c49a2d609b5b9958126f
Benjamin M Lancaster <bmlancaster07@gmail.com>
2018-10-11 00:18:28 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+board = [" ", " ", " "," ", " ", " "," ", " ", " "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +8,13 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(input)
+  puts "Where would you like to go?"
+input = gets.strip.to_i               #strip out whitespaces on the string and convert to int
+index = input - 1                     #convert to index value
+#return index                          #return index for use in other methods
+end
+
+def move (board,index)
+board[index] = "X"                    #set the value of the board's index to X
+end
