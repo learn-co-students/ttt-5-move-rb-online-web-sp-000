5f09d360a5fa4cec1e4e0097c51bbf50f7a5244b
Benjamin M Lancaster <bmlancaster07@gmail.com>
2018-10-11 02:15:36 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -13,6 +13,6 @@ index = user_input.to_i - 1          #convert to integer and covert to index
 return index                         #return index for use in other methods
 end
 
-def move (board,index)
+def move (board,index," ")
 board[index] = "X"                    #set the value of the board's index to X
 end
