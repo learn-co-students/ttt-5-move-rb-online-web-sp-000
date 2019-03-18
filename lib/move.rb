e1c48bde544c69acfada02fc20076c8e8dbd7077
mirthful-flag-5763 <irinagamb@gmail.com>
2019-03-18 01:24:40 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,14 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+# code your input_to_index and move method here!
+board = []
+
+def move(board, idx, value = 'X')
+  board[idx] = value
 end
 
-# code your input_to_index and move method here!
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def display_board(board)
+  puts " #{board[0]} |   |   "
+end
