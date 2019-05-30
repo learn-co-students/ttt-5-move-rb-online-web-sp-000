643ad37ead545cddd9d6e2eae1ac29617c374e76
github username <github email address>
2019-05-30 11:48:11 -0400

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -2,7 +2,7 @@ def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-" * 11  # instead of "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-" * 11
+  11.times { print "-"}
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
