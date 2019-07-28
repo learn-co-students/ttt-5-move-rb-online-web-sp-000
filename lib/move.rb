5d8c88b8bcf48a81586d76833be7f7cd1e10ea27
Michael Lynch <michael@michaelrlynch.com>
2019-07-28 16:24:52 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+# Display for board state
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,10 +8,15 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+# Convert user input to string and subtract 1 for ... computer counting (?)
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+# Takes arguments board, #input_to_index, and user_choice(with default value of X)
+# assigns board array the return value of input_to_index and sets it to string X.
+# This is where I am getting lost. I wrote it and know how it *SHOULD* work but
+# cannot figure out *HOW* it is working.
 def move(board, input_to_index, user_choice = "X")
   board[input_to_index] = user_choice
 end
