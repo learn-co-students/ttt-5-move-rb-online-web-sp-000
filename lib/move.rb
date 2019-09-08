00148e077162deeb9b3b231bad9401b18c10c377
Aubr R <a.renault2019@outlook.com>
2019-09-08 02:08:19 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -8,4 +8,10 @@ end
 
 # code your input_to_index and move method here!
 
-input_to_index 
+def input_to_index(user_input) 
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end  
