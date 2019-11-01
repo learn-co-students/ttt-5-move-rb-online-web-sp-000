87c73c375d2141654cd3f37d3979a3d8f0d59671
github username <github email address>
2019-10-31 22:49:25 -0400

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,9 @@
+def greeting
+
+  puts "Welcome to Tic Tac Toe!"
+
+end
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +13,20 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index
+
+  puts "Please input the position on the board you would like to fill"
+  input_position = gets.chomp
+  input_position.to_i
+
+end
+
+def move (array, index, value)
+
+end
+
+def update_array_with(array, index, value)
+/
+end
+
