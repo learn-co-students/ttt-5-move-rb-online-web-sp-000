da456f73ece86a6552d1061ac27eac9bd49b1470
Oliver Dahl <oliverpdahl@gmail.com>
2019-12-24 15:12:02 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,21 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+def display_board(boardIn)
+  if boardIn == nil
+    puts "That was an unacceptable input"
+  else
+    puts " #{boardIn[0]} | #{boardIn[1]} | #{boardIn[2]} "
+    puts "-----------"
+    puts " #{boardIn[3]} | #{boardIn[4]} | #{boardIn[5]} "
+    puts "-----------"
+    puts " #{boardIn[6]} | #{boardIn[7]} | #{boardIn[8]} "
+  end
 end
 
 # code your input_to_index and move method here!
+def input_to_index(input)
+  input_int = input.to_i - 1
+end
+  
+def move(boardIn, input_index, character = "X")
+  boardIn[input_index] = character
+  return boardIn
+end
