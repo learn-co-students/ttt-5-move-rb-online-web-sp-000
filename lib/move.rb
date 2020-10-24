4a00b37de677ce2cd38392eb79c480f2e09b7f76
Harrison Price <hprice518@gmail.com>
2020-10-24 01:10:01 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -8,6 +8,10 @@ end
 
 # code your input_to_index and move method here!
 
-def input_to_index(move)
-  move.to_i -1
+def input_to_index(input)
+  input.to_i-1
+end
+
+def move(board,input,draw) 
+  display_board[input_to_index(move)]
 end
