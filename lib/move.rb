82cd7c02d11e0cb24b5af7c0ab5276919efef9b7
Lj <tnedutsl457892@gmail.com>
2021-01-15 01:05:13 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(array, index, value)
-  return array[index] = value
+def move(board, index, value)
+  return board[index] = value
 end
-  
