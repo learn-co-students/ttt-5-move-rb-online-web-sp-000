138be348047b366f31c0fd10354f0ab20f9f1859
Technicallyaustin <Technicallyaustin@gmail.com>
2021-02-02 18:58:58 -0700

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,15 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(input)
+user_number = input.to_i
+reduced_user_number = user_number - 1
+reduced_user_number
+end
+
+def move(board, location, token = "X")
+  board[location] = token
+  board
+
+end
+
