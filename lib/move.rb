57c3520c273ac708da50181bd514ad6adae19a46
Kassandra Guerrero <kassandra-g@hotmail.com>
2021-02-08 00:10:39 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-puts "Welcome to Tic Tac Toe!"
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -9,6 +9,7 @@ end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 display_board(board)
+
 def input_to_index(user_input)
   index = user_input.to_i - 1
 end
